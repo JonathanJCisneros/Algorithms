2c0e787975cd8722838abe53bc4151d50cb2342e
Jonathan Cisneros <103544308+JonathanJCisneros@users.noreply.github.com>
2022-10-13 16:30:15 -0700

    Delete Interview.js

--- Algorithms/PostGrad/Interview.js
+++
@@ -1,9 +0,0 @@
-function LengthOfLastWord(s){
-    let trimmed = s.trim();
-    let newString = trimmed.split(' ')
-    return newString[newString.length-1].length
-}
-
-console.log(LengthOfLastWord('Hello World'))
-console.log(LengthOfLastWord("   fly me   to   the moon  "))
-console.log(LengthOfLastWord("luffy is still joyboy"))
